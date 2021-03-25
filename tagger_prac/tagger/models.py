e5f31d42a6301237dc5462478d0f48a158259c15
trogers19 <tnrogers019@gmail.com>
2021-03-25 17:42:21 -0400

    Update models.py

--- tagger_prac/tagger/models.py
+++ tagger_prac/tagger/models.py
@@ -14,7 +14,9 @@ class Roster(models.Model):
 class Player(models.Model):
     roster = models.ForeignKey(Roster, on_delete=models.CASCADE, null=True)
     player_name = models.CharField(max_length=50)
-    player_height = models.IntegerField(default=0)
+    player_number = models.IntegerField()
+    player_throwing_handedness = models.CharField(max_length=1)
+    player_batting_handedness = models.CharField(max_length=1)
 
     def get_absolute_url(self):
         return reverse('tagger:player_details', kwargs={'pk': self.pk})
@@ -23,15 +25,37 @@ class Player(models.Model):
 class Game(models.Model):
     game_title = models.CharField(max_length=200)
     start_time = models.DateTimeField('time started')
+    date = models.DatTimeField('date')
 
     def get_absolute_url(self):
         return reverse('tagger:game_details', kwargs={'pk': self.pk})
 
-# our Play model, which has whatever the play was
-class Play(models.Model):
+# our Inning model, which allows us to summarize games by inning
+class Inning(models.Model):
+    half = models.IntegerField() #this is a boolean for top (0) or bottom (1) of the inning
+    outs = models.IntegerField()
+    runs = models.IntegerField()
+    errors = models.IntegerField()
     game = models.ForeignKey(Game, on_delete=models.CASCADE, null=True)
-    play_type = models.CharField(max_length=200)
+
+# our atBat model, which relates pitches
+class AtBat(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, null=True)
     time_stamp = models.DateTimeField('time made')
+    result = models.CharField(max_length=200)
+    inning = models.ForeignKey(Inning, on_delete=models.CASCADE, null=True)
 
     def get_absolute_url(self):
-        return reverse('tagger:play_details', kwargs={'pk': self.pk})
+        return reverse('tagger:atbat_details', kwargs={'pk': self.pk})
+
+# our pitch model
+class Pitch(models.Model):
+    pitch_type = models.CharField()
+    pitch_call = models.CharField()
+    pitcher = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
+    batter = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
+    pitch_number = models.IntegerField()
+    atBat = models.ForeignKey(AtBat, on_delete=models.CASCADE, null=True)
+
+    def get_absolute_url(self):
+        return reverse('tagger:pitch_details', kwargs={'pk': self.pk})
